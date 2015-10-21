d94c2145c55c2454f9f42adac4cc0b015bde16fa
etrainexpress <kamhincheung@gmail.com>
2015-10-21 12:16:33 -0400

    init repo

---
+++ assets/gundam_anime/Gundam_anime_edge.js
@@ -0,0 +1,9 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x4='rgba(0,0,0,0)',e9='${Gundum1}',e10='${Gundum2}',g='image',o='opacity',x8='rgba(0,0,0,0.00)',i='none';var g5='Gundum3.png',g7='Gundum1.png',g6='Gundum2.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Gundum3',t:g,r:['0px','-3px','481px','260px','auto','auto'],f:[x4,im+g5,'0px','0px']},{id:'Gundum2',t:g,r:['0px','-3px','481px','260px','auto','auto'],o:'1',f:[x4,im+g6,'0px','0px']},{id:'Gundum1',t:g,r:['0px','-3px','481px','260px','auto','auto'],o:'1',f:[x4,im+g7,'0px','0px']}],style:{'${Stage}':{isStage:true,r:['null','null','481px','254px','auto','auto'],overflow:'hidden',f:[x8]}}},tt:{d:8000,a:y,data:[["eid1",o,1000,3000,"linear",e9,'1','0'],["eid3",o,5000,3000,"linear",e10,'1','0']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-92784730");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",8000,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-92784730");
